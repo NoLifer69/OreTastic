16d6db9ea175f51d629891a30f0815a850a433e1
Nolifer <loliamaoob@gmail.com>
2023-06-08 17:29:58 +0300

    Initial commit

---
+++ src/main/java/net/zeropercent/oretastic/OreTasticClient.java
@@ -0,0 +1,11 @@
+package net.zeropercent.oretastic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OreTasticClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
